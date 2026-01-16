606dc7e5b4a68e7b1a140b7b578a5cd4c44ebed2
Dj Denicore <113824079+DeniProtoshark@users.noreply.github.com>
2026-01-16 10:48:25 +0200

    Update weather_widget.py

--- weather_widget.py
+++ weather_widget.py
@@ -5,7 +5,7 @@ import time
 
 OPENWEATHER_API_KEY = "36028ec586574b08f1f58569db42961a"
 CITIES = [
-    "Tallinn,EE", "Tartu,EE", "Narva,EE", "Pärnu,EE", "Kohtla-Järve,EE", "Viljandi,EE", "Rakvere,EE", "Maardu,EE", "Sillamäe,EE", "Kuressaare,EE", "Haapsalu,EE"
+    "Tallinn,EE", "Haapsalu,EE", "Narva,EE", "Pärnu,EE", "Kohtla-Järve,EE", "Viljandi,EE", "Rakvere,EE", "Maardu,EE", "Sillamäe,EE", "Kuressaare,EE", "Tartu,EE"
 ]
 
 app = Flask(__name__)
@@ -187,3 +187,4 @@ def weather_api():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
+
